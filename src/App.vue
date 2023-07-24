6627606030fe1b1d6e6e679733e9da7a5e9bb4c9
Luca Membrini <130695419+MembriniLuca29@users.noreply.github.com>
2023-07-24 15:00:24 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import headerComponent from './components/headerComponent .vue';
+import mainComponent from './components/mainComponent.vue';
+import footerComponent from './components/footerComponent.vue'
+import axios from 'axios';
+import { store } from './store.js';
+
+export default {
+  name: "app",
+  components: {
+    headerComponent,
+    mainComponent,
+    footerComponent,
+  },
+  data() {
+    return {
+      store: store,
+    };
+  },
+};
+</script>
+
+<template>
+ <headerComponent/>
+  
+</template>
+
+<style lang="scss" scoped>
+@use "assets/scss/main";
+body{
+  margin: 0;
+}
+</style>
---
+++ src/components/headerComponent .vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+<div class="header-container">
+
+</div>
+</template>
+
+<style lang="scss" scoped>
+.header-container{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: black;
+    height: 200px;
+    width: 100%;
+}
+</style>
